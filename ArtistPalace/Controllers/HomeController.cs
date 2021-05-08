4c7e186031c5036cf4e56643d7e41654a1e1c888
anivire <anivire.work@gmail.com>
2021-05-08 20:22:54 +0300

    Finished work on adding artists to the temporary table (CheckoutArtists)

--- ArtistPalace/Controllers/HomeController.cs
+++ ArtistPalace/Controllers/HomeController.cs
@@ -1,73 +1,98 @@
 ﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using ArtistPalace.Data;
 using Microsoft.AspNetCore.Mvc;
 using ArtistPalace.Models;
 using Dapper;
+using Microsoft.Extensions.Logging;
 
 namespace ArtistPalace.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ConnectionFactory _connection;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ConnectionFactory connection)
+        public HomeController(ConnectionFactory connection, ILogger<HomeController> logger)
         {
             _connection = connection;
+            _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View("Index");
+            return View();
         }
 
-        public IActionResult SuggestArtists([FromQuery] SuggestArtistsQuery suggestArtistsQuery = null)
+        [HttpGet]
+        public IActionResult SuggestArtists()
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public IActionResult SuggestArtists([FromForm] SuggestArtistsQuery suggestArtistsQuery = null)
+        {
+            using (var connection = _connection.CreateConnection())
+            {
+                connection.Execute(
+                    "exec AddToCheckoutArtists @twitterTag, @type, @acceptCommissions, @pricePerHour, @isAccepted",
+                    new
+                    {
+                        twitterTag = suggestArtistsQuery.Tag,
+                        type = suggestArtistsQuery.Type,
+                        acceptCommissions = suggestArtistsQuery.AcceptCommissions,
+                        pricePerHour = suggestArtistsQuery.PricePerHour,
+                        isAccepted = false
+                    });
+            }
+            return View();
+        }
+
         public IActionResult Artists([FromQuery] ArtistsQuery artistsQuery = null)
         {
             using (var connection = _connection.CreateConnection())
             {
                 var builder = new SqlBuilder();
                 var template = builder.AddTemplate("select * from artists /**where**/");
-                
+
                 if (!string.IsNullOrWhiteSpace(artistsQuery?.NickName))
                 {
                     builder.Where($"Nickname Like '%{artistsQuery.NickName}%'");
                 }
-                
+
                 if (!string.IsNullOrWhiteSpace(artistsQuery?.Country) && artistsQuery?.Country != "All")
                 {
                     builder.Where($"Country = '{artistsQuery.Country}'");
                 }
-                
+
                 if (!string.IsNullOrWhiteSpace(artistsQuery?.Type) && artistsQuery?.Type != "All")
                 {
                     builder.Where($"Type = '{artistsQuery.Type}'");
                 }
-                
+
                 if (!string.IsNullOrWhiteSpace(artistsQuery?.Rank) && artistsQuery?.Rank != "All")
                 {
                     builder.Where($"Rank = '{artistsQuery.Rank}'");
                 }
-                
+
                 if (!string.IsNullOrWhiteSpace(artistsQuery?.Commissions) && artistsQuery?.Commissions != "All")
                 {
                     builder.Where($"AcceptCommissions = '{(artistsQuery.Commissions == "Yes" ? true : false)}'");
                 }
-
-                // debug
-                Console.WriteLine(template.RawSql.ToString());
                 
+                _logger.Log(LogLevel.Information, template.RawSql.ToString());
+
                 var artists = connection.Query<Artist>(template.RawSql.ToString()).ToList();
                 return View(artists);
             }
         }
-        
+
         public IActionResult AdminPanel()
         {
             return View();
--- ArtistPalace/Models/SuggestArtistsQuery.cs
+++ ArtistPalace/Models/SuggestArtistsQuery.cs
@@ -3,6 +3,7 @@
     public class SuggestArtistsQuery
     {
         public string Tag { get; set; }
+        public string Type { get; set; }
         public string AcceptCommissions { get; set; }
         public string PricePerHour { get; set; }
     }
