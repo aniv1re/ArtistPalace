5b62412af5bb14764b574634f63cb83e79c7e52f
anivire <anivire.work@gmail.com>
2021-06-06 21:35:21 +0300

    Change home page from Home to Artists

--- ArtistPalace/Startup.cs
+++ ArtistPalace/Startup.cs
@@ -55,7 +55,7 @@ namespace ArtistPalace
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Artists}/{action=Index}/{id?}");
             });
         }
     }
